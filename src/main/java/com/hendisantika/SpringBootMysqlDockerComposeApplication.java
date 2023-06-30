54dfb8f4777d47faba13ccda9c2078f19e93a5fc
hendisantika <hendisantika@yahoo.co.id>
2023-06-30 10:05:21 +0700

    #6 - Add SpringBootServletInitializer ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/SpringBootMysqlDockerComposeApplication.java
+++ src/main/java/com/hendisantika/SpringBootMysqlDockerComposeApplication.java
@@ -2,12 +2,19 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootMysqlDockerComposeApplication {
+public class SpringBootMysqlDockerComposeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMysqlDockerComposeApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootMysqlDockerComposeApplication.class);
     }
 
 }
