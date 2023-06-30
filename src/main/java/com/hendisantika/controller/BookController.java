d35191b637e2b1a7934ea9cb2b36839715f41ed3
hendisantika <hendisantika@yahoo.co.id>
2023-06-30 10:06:34 +0700

    #8 - Add displayForm in BookController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/controller/BookController.java
+++ src/main/java/com/hendisantika/controller/BookController.java
@@ -1,10 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Book;
 import com.hendisantika.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +28,18 @@ public class BookController {
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     private final BookRepository bookRepository;
+
+    @GetMapping("/")
+    public String displayForm(Model model) {
+        logger.info("*** displayForm () method execution start ***");
+        Book bookObj = new Book();
+        model.addAttribute("book", bookObj);
+
+        List<Book> booksList = bookRepository.findAll();
+        model.addAttribute("books", booksList);
+
+        logger.info("*** displayForm () method execution end ***");
+        return "index";
+    }
+
 }
